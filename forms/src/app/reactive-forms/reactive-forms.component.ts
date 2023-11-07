4c8ffdc175a33237b46d225b36dd7ff252a90b98
unknown <madhuspediredla@gmail.com>
2023-11-07 15:24:38 +0530

    reactive forms completed

--- forms/src/app/reactive-forms/reactive-forms.component.ts
+++ forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
   styleUrls: ['./reactive-forms.component.scss']
 })
-export class ReactiveFormsComponent {
-
-  gender
+export class ReactiveFormsComponent implements OnInit {
+  
+  genders =['male', 'female']
   signupForm!: FormGroup;
+  
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'firstName': new FormControl(null, Validators.required),
+        'lastName': new FormControl(null)
+      }),
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    })
 
+    this.signupForm.setValue({
+      'userData': {
+        firstName: 'Madhu Sudhan',
+        lastName: 'Pediredla'
+      },
+      'username': 'madhu',
+      'email':'madhu@gmail.com',
+      'gender': 'male',
+      'hobbies':[]
+    })
+  }
+
+  onSubmit(){
+    console.log(this.signupForm)
+  }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (this.signupForm.get('hobbies') as FormArray).controls;
+  }
 }
