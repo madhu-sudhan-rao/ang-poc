be0b17fd1c5401dba3a0d83d4b6285d5531410a3
unknown <madhuspediredla@gmail.com>
2023-11-22 21:24:14 +0530

    http requests project added

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'users',
     component: UsersComponent
+  },
+  {
+    path: 'users/:id',
+    component: UserComponent
   }
 ];
 
