34a7aab48bef86997e87319247095377c9d32b31
unknown <madhuspediredla@gmail.com>
2023-12-18 20:15:29 +0530

    blank

--- http-requests/src/app/posts.service.ts
+++ http-requests/src/app/posts.service.ts
@@ -16,7 +16,77 @@ export class PostsService {
   ) { }
 
   createAndStorePosts(title: string, content: string){
-    const postData: Post = {title: title, content: content}
+    const postData: any = {message: 'your rates',
+    data: {
+      rates: {
+        greenScreens: {
+          targetBuyRate: '1.459',
+          lowBuyRate: '1.366',
+          highBuyRate: '1.565',
+          startBuyRate: '1.3375',
+          fuelRate: '0.43',
+          confidenceLevel: '87',
+          networkRates: {
+            targetBuyRate: '1.459',
+            lowBuyRate: '1.366',
+            highBuyRate: '1.565',
+            startBuyRate: '1.3375',
+            fuelRate: '0.43',
+            confidenceLevel: '87',
+          },
+        },
+        dat: {
+          mileage: 2025,
+          reports: 15,
+          companies: 8,
+          standardDeviation: 0.28,
+          perMile: {
+            rateUsd: 1.02,
+            highUsd: 1.14,
+            lowUsd: 0.72,
+          },
+          averageFuelSurchargePerMileUsd: 0.47,
+          perTrip: {
+            rateUsd: 2065.5,
+            highUsd: 2308.5,
+            lowUsd: 1458.0,
+          },
+          averageFuelSurchargePerTripUsd: 0.47,
+          escalation: {
+            timeframe: '7_DAYS',
+            origin: {
+              name: '606xx',
+              type: '3_DIGIT_ZIP',
+            },
+            destination: {
+              name: 'Los Angeles Mkt',
+              type: 'MARKET_AREA',
+            },
+          },
+        },
+        request: {
+          origin: {
+            city: 'Chicago',
+            stateOrProvince: 'IL',
+            postalCode: '60611',
+          },
+          destination: {
+            city: 'Los Angeles',
+            stateOrProvince: 'CA',
+            postalCode: '90001',
+          },
+          equipment: 'VAN',
+          includeMyRate: false,
+          targetEscalation: {
+            escalationType: 'BEST_FIT',
+          },
+          rateType: 'SPOT',
+          rateTimePeriod: {
+            rateTense: 'CURRENT',
+          },
+        },
+      },
+    },}
     this.http.post(
       'https://angular-firebase-2-e9201-default-rtdb.firebaseio.com/posts.json',
       postData,
